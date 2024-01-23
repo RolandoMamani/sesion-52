26c42a7190eb42aa73fc843730c0497a125e01b2
RolandoMamani <94076965+RolandoMamani@users.noreply.github.com>
2024-01-22 19:04:59 -0500

    Add files via upload

---
+++ jquery/query.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    // alert("hola mundo desde query");
+    // let varbotoon=$('button').html();
+    //console.log(varbotoon);
+    //$('button').html("quiero otro texto");
+    //$('.btn').html("cambiado desde la clase");
+   // $('#boton').html("cambiado desde su id");
+}
+)
+document.querySelector('#boton').innerHTML='cambiado desde js';
